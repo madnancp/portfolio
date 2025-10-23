caad98ffc9e9d3e894e3c231164c62dbab8c21aa
Adnan <adnanmuhammedadnancp@gmail.com>
2025-10-24 01:08:32 +0530

    add featured project section

---
+++ app/(home)/sections/FeaturedProjects.tsx
@@ -0,0 +1,11 @@
+import { SectionTitle } from "@/components/SectionTitle";
+
+const FeaturedProjects = () => {
+  return (
+    <section>
+      <SectionTitle title="Featured Projects" />
+    </section>
+  )
+}
+
+export default FeaturedProjects;
